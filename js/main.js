954415a5f7fc50d95210dd89124da4d77999b5fc
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-20 18:40:05 -0500

    feat: :sparkles: Obtencion de data e insercion en el html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+import { userList } from "./components/list.js";
 import { getAllUsers } from "./module/app.js";
 
+let live_filter__list = document.querySelector(".live_filter__list")
+
 console.table( await getAllUsers());
+let data = await getAllUsers()
+console.log(await userList(data))
+live_filter__list.innerHTML = await userList(data);
