e62561cc4d71cbf5c8776e17049648f894eb8213
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-20 19:21:32 -0500

    feat: :sparkles: implementación de filtrado en vivo para la lista de usuarios
    Se ha agregado la funcionalidad de filtrado en vivo al componente. Esto permite a los usuarios buscar usuarios específicos en la lista y filtrar resultados no deseados.
    
    feat: :lipstick: estilo para ocultar elementos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 import { userList } from "./components/list.js";
 import { getAllUsers } from "./module/app.js";
 
-let live_filter__list = document.querySelector(".live_filter__list")
+let live_filter__list = document.querySelector(".live_filter__list");
+let live_filter__input = document.querySelector(".live_filter__input");
+let live_filter__list_items = [];
 
-console.table( await getAllUsers());
-let data = await getAllUsers()
-console.log(await userList(data))
+live_filter__input.addEventListener("input", (e) => filterData(e.target.value));
+
+let data = await getAllUsers();
+console.table(data);
 live_filter__list.innerHTML = await userList(data);
+live_filter__list_items = live_filter__list.querySelectorAll(".live_filter__list_item");
+
+
+
+let filterData = (searchTerm) => {
+    live_filter__list_items.forEach((item) => {
+        if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.remove("hide");
+        } else {
+            item.classList.add("hide");
+        }
+    });
+}
